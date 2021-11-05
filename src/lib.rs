6e0b3d811c64bfb8a782ed8d49610a0519b6126e
Andrew Walbran <qwandor@google.com>
2021-11-05 12:51:19 +0000

    Describe ISS for RES0 cases.

--- src/lib.rs
+++ src/lib.rs
@@ -311,6 +311,17 @@ pub struct Decoded {
     pub fields: Vec<FieldInfo>,
 }
 
+fn decode_iss_res0(iss: u64) -> Result<Decoded, DecodeError> {
+    if iss == 0 {
+        Ok(Decoded {
+            description: Some("ISS is RES0".to_string()),
+            fields: vec![],
+        })
+    } else {
+        Err(DecodeError::InvalidRes0 { res0: iss })
+    }
+}
+
 pub fn decode(esr: u64) -> Result<Decoded, DecodeError> {
     let res0 = FieldInfo::get(esr, "RES0", 37, 64).check_res0()?;
     let iss2 = FieldInfo::get(esr, "ISS2", 32, 37);
@@ -318,7 +329,7 @@ pub fn decode(esr: u64) -> Result<Decoded, DecodeError> {
     let il = FieldInfo::get_bit(esr, "IL", 25);
     let iss = FieldInfo::get(esr, "ISS", 0, 25);
     let (class, iss_decoded) = match ec.value {
-        0b000000 => ("Unknown reason", None),
+        0b000000 => ("Unknown reason", Some(decode_iss_res0(iss.value)?)),
         0b000001 => ("Wrapped WF* instruction execution", None),
         0b000011 => ("Trapped MCR or MRC access with coproc=0b1111", None),
         0b000100 => ("Trapped MCRR or MRRC access with coproc=0b1111", None),
@@ -328,18 +339,18 @@ pub fn decode(esr: u64) -> Result<Decoded, DecodeError> {
         0b001010 => ("Trapped execution of an LD64B, ST64B, ST64BV, or ST64BV0 instruction", None),
         0b001100 => ("Trapped MRRC access with (coproc==0b1110)", None),
         0b001101 => ("Branch Target Exception", None),
-        0b001110 => ("Illegal Execution state", None),
+        0b001110 => ("Illegal Execution state", Some(decode_iss_res0(iss.value)?)),
         0b010001 => ("SVC instruction execution in AArch32 state", None),
         0b010101 => ("SVC instruction execution in AArch64 state", None),
         0b011000 => ("Trapped MSR, MRS or System instruction execution in AArch64 state", None),
-        0b011001 => ("Access to SVE functionality trapped as a result of CPACR_EL1.ZEN, CPTR_EL2.ZEN, CPTR_EL2.TZ, or CPTR_EL3.EZ", None),
+        0b011001 => ("Access to SVE functionality trapped as a result of CPACR_EL1.ZEN, CPTR_EL2.ZEN, CPTR_EL2.TZ, or CPTR_EL3.EZ", Some(decode_iss_res0(iss.value)?)),
         0b011100 => ("Exception from a Pointer Authentication instruction authentication failure", None),
         0b100000 => ("Instruction Abort from a lower Exception level", Some(decode_iss_instruction_abort(iss.value)?)),
         0b100001 => ("Instruction Abort taken without a change in Exception level", Some(decode_iss_instruction_abort(iss.value)?)),
-        0b100010 => ("PC alignment fault exception", None),
+        0b100010 => ("PC alignment fault exception", Some(decode_iss_res0(iss.value)?)),
         0b100100 => ("Data Abort from a lower Exception level", Some(decode_iss_data_abort(iss.value)?)),
         0b100101 => ("Data Abort taken without a change in Exception level", Some(decode_iss_data_abort(iss.value)?)),
-        0b100110 => ("SP alignment fault exception", None),
+        0b100110 => ("SP alignment fault exception", Some(decode_iss_res0(iss.value)?)),
         0b101000 => ("Trapped floating-point exception taken from AArch32 state", None),
         0b101100 => ("Trapped floating-point exception taken from AArch64 state", None),
         0b101111 => ("SError interrupt", None),
