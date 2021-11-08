170b2ccbf3c56db6406afedcd308de7f2fd9f5ca
Andrew Walbran <qwandor@google.com>
2021-11-08 14:37:45 +0000

    Show top-level of result in table.

--- aarch64-esr-web/src/lib.rs
+++ aarch64-esr-web/src/lib.rs
@@ -12,9 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use aarch64_esr_decoder::decode;
+use aarch64_esr_decoder::{decode, Decoded};
 use std::num::ParseIntError;
 use wasm_bindgen::prelude::*;
+use web_sys::{Document, Element};
 
 #[wasm_bindgen]
 pub fn init() {
@@ -26,10 +27,90 @@ pub fn init() {
 pub fn decode_esr(esr: &str) -> String {
     if let Ok(esr) = parse_number(esr) {
         let decoded = decode(esr);
+        if let Ok(decoded) = &decoded {
+            show_decoded(esr, decoded);
+        }
         format!("{:?}", decoded)
     } else {
         "Invalid ESR".to_string()
     }
+}
+
+fn make_cell(
+    document: &Document,
+    contents: Option<&str>,
+    colspan: usize,
+) -> Result<Element, JsValue> {
+    let cell = document.create_element("td")?;
+    cell.set_attribute("colspan", &colspan.to_string())?;
+    cell.set_text_content(contents);
+    Ok(cell)
+}
+
+fn show_decoded(esr: u64, decoded: &Decoded) -> Result<(), JsValue> {
+    let document = web_sys::window()
+        .expect("Couldn't find window")
+        .document()
+        .expect("Couldn't find document");
+    let table = document
+        .get_element_by_id("result_table")
+        .expect("Couldn't find result table");
+    // Remove existing contents.
+    table.set_inner_html("");
+
+    // First row has ESR in binary
+    let row = document.create_element("tr")?;
+    for i in (0..u64::BITS).rev() {
+        let bit = esr & (1 << i) != 0;
+        let cell = make_cell(&document, Some(if bit { "1" } else { "0" }), 1)?;
+        row.append_child(&cell)?;
+    }
+    table.append_child(&row)?;
+
+    // Top-level description
+    let row = document.create_element("tr")?;
+    let cell = make_cell(&document, decoded.description.as_deref(), 64)?;
+    row.append_child(&cell)?;
+    table.append_child(&row)?;
+
+    // Top-level field names
+    let row = document.create_element("tr")?;
+    let mut last = 64;
+    for field in &decoded.fields {
+        if field.start + field.width != last {
+            // Add a filler
+            let cell = make_cell(&document, None, last - field.start - field.width)?;
+            row.append_child(&cell)?;
+        }
+        let cell = make_cell(&document, Some(field.name), field.width)?;
+        row.append_child(&cell)?;
+        last = field.start;
+    }
+    table.append_child(&row)?;
+
+    // Top-level field descriptions
+    let row = document.create_element("tr")?;
+    let mut last = 64;
+    for field in &decoded.fields {
+        if field.start + field.width != last {
+            // Add a filler
+            let cell = make_cell(&document, None, last - field.start - field.width)?;
+            row.append_child(&cell)?;
+        }
+        let cell = make_cell(
+            &document,
+            field
+                .decoded
+                .as_ref()
+                .and_then(|decoded| decoded.description.as_deref()),
+            field.width,
+        )?;
+        row.append_child(&cell)?;
+        last = field.start;
+    }
+    table.append_child(&row)?;
+
+    Ok(())
 }
 
 /// Parse a decimal or hexadecimal number.
