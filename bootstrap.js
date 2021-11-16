b89ead98570c6836b684b415d23c7eb30dcff932
qwandor <qwandor@google.com>
2021-11-16 17:14:17 +0000

    Deploying to gh-pages from @ google/aarch64-esr-decoder@41b7d2180f4c0a0b6d8897283ca06778c2e050bc 🚀

--- bootstrap.js
+++ bootstrap.js
@@ -222,7 +222,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/aarch64_esr_web_bg.wasm":"2f3d144e039409c57b33"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/aarch64_esr_web_bg.wasm":"2a5afbe4742dfb649c15"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
