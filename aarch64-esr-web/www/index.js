831964f5f959ac25f56aaa304557626e144e454d
Andrew Walbran <qwandor@google.com>
2021-11-18 15:25:24 +0000

    Autoformat JavaScript.

--- aarch64-esr-web/www/index.js
+++ aarch64-esr-web/www/index.js
@@ -14,17 +14,17 @@
 
 import * as wasm from "aarch64-esr-web";
 
-wasm.init()
+wasm.init();
 
 const esr = document.getElementById("esr");
 esr.oninput = () => {
-    if (esr.value.length > 0) {
-        wasm.decode_esr(esr.value)
-    }
-    window.location.hash = esr.value
+  if (esr.value.length > 0) {
+    wasm.decode_esr(esr.value);
+  }
+  window.location.hash = esr.value;
 };
 
 if (window.location.hash) {
-    esr.value = window.location.hash.substring(1)
-    wasm.decode_esr(esr.value)
+  esr.value = window.location.hash.substring(1);
+  wasm.decode_esr(esr.value);
 }
