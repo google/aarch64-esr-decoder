420f2c7dc07845f2089599590c1e7e497054c4c3
Andrew Walbran <qwandor@google.com>
2022-04-13 14:38:50 +0100

    Copy midr.html too.

--- aarch64-esr-web/www/webpack.config.js
+++ aarch64-esr-web/www/webpack.config.js
@@ -1,5 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: "./bootstrap.js",
@@ -8,7 +8,5 @@ module.exports = {
     filename: "bootstrap.js",
   },
   mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+  plugins: [new CopyWebpackPlugin(["index.html", "midr.html"])],
 };
