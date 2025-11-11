0b3a402dc0653c24b8b18758cfd1fdf25ecb7ed3
Andrew Walbran <qwandor@google.com>
2025-11-11 18:00:08 +0000

    Add doc comment to top-level module.

--- arm-sysregs-xml/src/lib.rs
+++ arm-sysregs-xml/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Types to parse the Arm system register XML format.
+
 pub mod json;
 
 use serde::{Deserialize, Deserializer, de::Unexpected};
